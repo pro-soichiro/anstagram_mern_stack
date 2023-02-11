9cbaa5c401ad5cd655d959f219f8f78d2a4badb4
pro-soichiro <dr.1987626@gmail.com>
2023-02-11 19:09:22 +0900

    styled users page

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,19 +1,50 @@
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const StyledCard = styled.div`
+  display: inline-block;
+  padding: 10px;
+  width: 330px;
+  border-radius: 5px;
+  border: 2px solid var(--d);
+  box-shadow: var(--shadow-sp);
+  flex-basis: 330px;
+  flex-shrink: 1;
+
+  :hover {
+    box-shadow: none;
+  }
+`;
+
+const NameKana = styled.p`
+  font-size: 10px;
+  color: grey;
+  margin: 0;
+`;
+
+const Name = styled.p`
+  font-size: 16px;
+  font-weight: 700;
+  margin: 5px 0;
+`;
+
+const Dep = styled.p`
+  font-size: 14px;
+  margin: 10px 0;
+`;
 
 const Card = ({ user }) => {
   return (
     <Link to={`/users/${user._id}`}>
-      <h3>
-        {user.lastName} {user.firstName}
-      </h3>
-      <p>
-        {user.lastNameKana} {user.firstNameKana}
-      </p>
-
-      <div>
-        <div>部署</div>
-        <p>{user.department}</p>
-      </div>
+      <StyledCard>
+        <NameKana>
+          {user.lastNameKana} {user.firstNameKana}
+        </NameKana>
+        <Name>
+          {user.lastName} {user.firstName}
+        </Name>
+        <Dep>部署: {user.department}</Dep>
+      </StyledCard>
     </Link>
   );
 };
--- src/pages/Users.js
+++ src/pages/Users.js
@@ -1,19 +1,28 @@
 import { useUsers } from "../contexts/UserContext";
+import styled from "styled-components";
 
 import Card from "../components/Card";
+
+const CardContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  margin: 30px auto;
+  justify-content: center;
+  gap: 20px;
+`;
 
 const Users = () => {
   const users = useUsers();
 
   return (
-    <>
-      <h1>ユーザー一覧</h1>
-      <div>
+    <div className="container">
+      <h1 className="page-title">ユーザー一覧</h1>
+      <CardContainer>
         {users.map((user) => (
           <Card key={user._id} user={user} />
         ))}
-      </div>
-    </>
+      </CardContainer>
+    </div>
   );
 };
 
