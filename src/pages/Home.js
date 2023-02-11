81e844031fd14c14b935daab6c513a674ceb0a37
pro-soichiro <dr.1987626@gmail.com>
2023-02-11 18:47:03 +0900

    styled home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,17 @@
+import styled from "styled-components";
+
+const Description = styled.p`
+  text-align: center;
+`
+
 const Home = () => {
 
   return (
-    <>
-     <h1>Home Component</h1>
-    </>
+    <div className="container">
+
+      <h1 className="page-title">anstagram</h1>
+      <Description>これはアン・コンサルティングで働くメンバーのプロフィール帳です！</Description>
+    </div>
   );
 }
 
