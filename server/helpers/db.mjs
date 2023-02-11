c3cfa11dc40e31f49aa35b4f769b33eaeabfef71
pro-soichiro <dr.1987626@gmail.com>
2023-02-12 06:22:39 +0900

    add mongoose settings

--- server/helpers/db.mjs
+++ server/helpers/db.mjs
@@ -2,6 +2,8 @@ import mongoose from "mongoose";
 import env from "dotenv";
 env.config();
 
+mongoose.set("strictQuery", false);
+
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
