cf9562aacb0f2afb7832f88cccdbb4f29644d4b5
pro-soichiro <dr.1987626@gmail.com>
2023-02-11 11:13:39 +0900

    add deploy settings

--- server/app.mjs
+++ server/app.mjs
@@ -1,20 +1,27 @@
-import express from "express"
-import env from "dotenv"
-import "./helpers/db.mjs"
+import path from "path";
+import express from "express";
+import env from "dotenv";
+import "./helpers/db.mjs";
 env.config();
 
-import apiRoutes from "./api-routes/index.mjs"
+import apiRoutes from "./api-routes/index.mjs";
 
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use(express.json())
+app.use(express.static("build"));
+app.use(express.json());
 
 // API
-app.use('/api', apiRoutes);
+app.use("/api", apiRoutes);
 
-app.use(function(req, res) {
-  res.status(404).json({ msg: "Page Not Found" })
+app.get("*", function (req, res) {
+  const indexHtml = path.resolve("build", "index.html");
+  res.sendFile(indexHtml);
+});
+
+app.use(function (req, res) {
+  res.status(404).json({ msg: "Page Not Found" });
 });
 
 app.use(function (err, req, res, next) {
@@ -24,6 +31,6 @@ app.use(function (err, req, res, next) {
   res.status(500).json({ msg: "不正なエラーが発生しました。" });
 });
 
-app.listen(port, function() {
-  console.log(`Server Start: http://localhost:${port}`)
-})
+app.listen(port, function () {
+  console.log(`Server Start: http://localhost:${port}`);
+});
